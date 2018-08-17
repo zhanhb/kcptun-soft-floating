162ee5bc6b1cc44c62a74c412f448e11211b689f
zhanhb <hzzhb@tairanchina.com>
2018-08-17 13:16:17 +0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+GO_VERSION=1.10.3
+UPX_VERSION=3.94
+wget https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+tar -xf go${GO_VERSION}.linux-amd64.tar.gz
+wget https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-amd64_linux.tar.xz
+tar -xf upx-${UPX_VERSION}-amd64_linux.tar.xz
+wget https://dl.google.com/go/go${GO_VERSION}.src.tar.gz
+tar -xf go${GO_VERSION}.src.tar.gz
+export PATH="`pwd`/go/bin:$PATH"
+export PATH="`pwd`/upx-${UPX_VERSION}-amd64_linux:$PATH"
+export GOPATH="`pwd`/gopath"
+export PATH="$GOPATH/bin:$PATH"
+
